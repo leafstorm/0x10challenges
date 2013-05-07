b3cfe80188eda16b14426e8b9de7eaf2b9023b1a
Matthew Frazier <leafstormrush@gmail.com>
2013-05-06 20:23:06 -0400

    added logging to the launcher

--- zx10challenges/launcher.py
+++ zx10challenges/launcher.py
@@ -7,6 +7,10 @@ This is a WSGI launcher for Heroku-like environments.
 :copyright: (C) 2013 Matthew Frazier
 :license:   MIT/X11 -- see the LICENSE file for details
 """
+import logging
+import sys
+from logging import StreamHandler
 from .application import create_app
 
 app = create_app()
+app.logger.addHandler(StreamHandler(sys.stderr))
